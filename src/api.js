2efaab56eb89d10180811d85b88aee475f83bdc5
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-10-29 17:57:53 +0700

    added redirects netlify.toml

--- src/api.js
+++ src/api.js
@@ -2,12 +2,11 @@ const express = require("express");
 const serverless = require("serverless-http");
 
 const app = express();
-
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.json({
-    hello: "hi zizsy!",
+    hello: "hi Ilham!!!",
   });
 });
 
@@ -17,6 +16,7 @@ router.get("/test", (req, res) => {
   });
 });
 
-app.use("/.netlify/functions/api", router);
+// app.use("/.netlify/functions/api", router);
+app.use("/", router);
 
 module.exports.handler = serverless(app);
